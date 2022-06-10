2af0369bd4cb311404baf8a49168915d9c41f4fc
AlMaz <almaz8811@gmail.com>
2022-06-11 01:29:22 +0300

    Разбил по пакетам

---
+++ handlers/client.py
@@ -0,0 +1,31 @@
+from aiogram import types, Dispatcher
+from create_bot import dp, bot
+
+# @dp.message_handler(commands=['start', 'help'])
+async def command_start(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, 'Приятного аппетита!')
+        await message.delete()
+    except:
+        await message.reply('Общение с ботом через ЛС, напишите ему: \nhttp://t.me/almaz8811PHub_bot')\
+
+# @dp.message_handler(commands=['Режим_работы'])
+async def pizza_open_command(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Вс-Чт с 9:00 до 20:00б Пт-Сб с 10:00 до 23:00')
+
+# @dp.message_handler(commands=['Расположение'])
+async def pizza_place_command(message: types.Message):
+    await bot.send_message(message.from_user.id, 'ул. Колбасная 15')
+
+# @dp.message_handler()
+# async def echo_send(message: types.Message):
+#     if message.text == 'Привет':
+#         await message.answer('И тебе привет!')
+#     await message.answer(message.text)
+#     await message.reply(message.text)
+#     await bot.send_message(message.from_user.id, message.text)
+
+def register_handlers_client(dp: Dispatcher):
+    dp.register_message_handler(command_start, commands=['start', 'help'])
+    dp.register_message_handler(pizza_open_command, commands=['Режим_работы'])
+    dp.register_message_handler(pizza_place_command, commands=['Расположение'])
