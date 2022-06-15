a4d7a0383acc5167a9c98c2249af86fd2dedc7c3
AlMaz <almaz8811@gmail.com>
2022-06-15 03:16:02 +0300

    Добавиль проверку на модератора

--- handlers/admin.py
+++ handlers/admin.py
@@ -2,7 +2,9 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types, Dispatcher
-from create_bot import dp
+from create_bot import dp, bot
+
+ID = None
 
 class FSMAdmin(StatesGroup):
     photo = State()
@@ -11,29 +13,37 @@ class FSMAdmin(StatesGroup):
     price = State()
 
 # Получаем ID текущего модератора
-
+# @dp.message_handler(commands='moderator', is_chat_admin=True)
+async def make_changes_command(message: types.Message):
+    global ID
+    ID = message.from_user.id
+    await bot.send_message(message.from_user.id, 'Что хозяин надо???') #, reply_markup=button_case_admin)
+    await message.delete()
 
 # Начало диалога загрузки нового пункта меню
 # @dp.message_handler(commands='Загрузить', state=None)
 async def cm_start(message: types.Message):
-    await FSMAdmin.photo.set()
-    await message.reply('Загрузи фото')
+    if message.from_user.id == ID:
+        await FSMAdmin.photo.set()
+        await message.reply('Загрузи фото')
 
 # Ловим первый ответ и пишем в словарь
 # @dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
 async def load_photo(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['photo'] = message.photo[0].file_id
-    await FSMAdmin.next()
-    await message.reply('Теперь введи название')
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['photo'] = message.photo[0].file_id
+        await FSMAdmin.next()
+        await message.reply('Теперь введи название')
 
 # Ловим второй ответ
 # @dp.message_handler(state=FSMAdmin.name)
 async def load_name(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['name'] = message.text
-    await FSMAdmin.next()
-    await message.reply('Введи описание')
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['name'] = message.text
+        await FSMAdmin.next()
+        await message.reply('Введи описание')
 
 # Ловим третий ответ
 # @dp.message_handler(state=FSMAdmin.description)
@@ -46,21 +56,23 @@ async def load_description(message: types.Message, state: FSMContext):
 # Ловим последний ответ и используем полученные данные
 # @dp.message_handler(state=FSMAdmin.price)
 async def load_price(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['price'] = float(message.text)
-    async with state.proxy() as data:
-        await message.reply(str(data))
-    await state.finish()
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['price'] = float(message.text)
+        async with state.proxy() as data:
+            await message.reply(str(data))
+        await state.finish()
 
 # Выход из состояний
 # @dp.message_handler(state='*', commands='отмена')
 # @dp.message_handler(Text(equals='отмена', ignore_case=True), state='*')
 async def cancel_handler(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    await state.finish()
-    await message.reply('OK')
+    if message.from_user.id == ID:
+        current_state = await state.get_state()
+        if current_state is None:
+            return
+        await state.finish()
+        await message.reply('OK')
 
 #Регистрируем хэндлеры
 def register_handlers_admin(dp: Dispatcher):
@@ -71,3 +83,4 @@ def register_handlers_admin(dp: Dispatcher):
     dp.register_message_handler(load_price, state=FSMAdmin.price)
     dp.register_message_handler(cancel_handler, state='*', commands='отмена')
     dp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state='*')
+    dp.register_message_handler(make_changes_command, commands='moderator', is_chat_admin=True)
